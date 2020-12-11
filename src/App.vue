74156f2d75beb84c2d9ac6583b98009f15325d31
kaito <kaitooshiro1@icloud.com>
2020-12-12 00:08:45 +0900

    ログイン・ログアウトボタン修正

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,218 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header class="header">
+      <h1>Chat</h1>
+      <!-- ログイン時にはフォームとログアウトボタンを表示 -->
+      <div v-if="user.uid" key="login">
+        [{{ user.displayName }}]
+        <button class="btn-flat-dashed-border" type="button" @click="doLogout">ログアウト</button>
+      </div>
+      <!-- 未ログイン時にはログインボタンを表示 -->
+      <div v-else key="logout">
+        <button class="btn-flat-dashed-border" type="button" @click="doLogin">ログイン</button>
+      </div>
+    </header>
+
+    <!--Firebase から取得したリストを描画（トランジション付き）-->
+    <transition-group name="chat" tag="div" class="list content">
+      <section
+              v-for="{ key, name, image, message } in chat"
+              :key="key"
+              class="item"
+      >
+        <div class="item-image">
+          <img :src="image" width="40" height="40" />
+        </div>
+        <div class="item-detail">
+          <div class="item-name">{{ name }}</div>
+          <div class="item-message">
+            <nl2br tag="div" :text="message" />
+          </div>
+        </div>
+      </section>
+    </transition-group>
+
+    <!-- 入力フォーム -->
+    <form action="" @submit.prevent="doSend" class="form">
+      <textarea
+              v-model="input"
+              :disabled="!user.uid"
+              @keydown.enter.exact.prevent="doSend"
+      ></textarea>
+      <button type="submit" :disabled="!user.uid" class="send-button">
+        Send
+      </button>
+    </form>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .header {
+    background: #0e5cb3;
+    margin-bottom: 1em;
+    padding: 0.4em 0.8em;
+    color: #f6fcff;
+  }
+  .btn-flat-dashed-border {
+    display: inline-block;
+    padding: 0.3em 0.7em;
+    margin: 0.3em 0.7em;
+    text-decoration: none;
+    color: #1b2236;
+    border: dashed 1px #67c5ff;
+    border-radius: 3px;
+    transition: .4s;
+  }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .btn-flat-dashed-border:hover {
+    border-style: dotted;
+    color: #679efd;
+  }
+  .content {
+    margin: 0 auto;
+    padding: 0 10px;
+    max-width: 600px;
+  }
+  .form {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    bottom: 0;
+    height: 80px;
+    width: 100%;
+    background: #f5f5f5;
+  }
+  .form textarea {
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    height: 4em;
+    width: calc(100% - 6em);
+    resize: none;
+  }
+  .list {
+    margin-bottom: 100px;
+  }
+  .item {
+    position: relative;
+    display: flex;
+    align-items: flex-end;
+    margin-bottom: 0.8em;
+  }
+  .item-image img {
+    border-radius: 20px;
+    vertical-align: top;
+  }
+  .item-detail {
+    margin: 0 0 0 1.4em;
+  }
+  .item-name {
+    font-size: 75%;
+  }
+  .item-message {
+    position: relative;
+    display: inline-block;
+    padding: 0.8em;
+    background: #deefe8;
+    border-radius: 4px;
+    line-height: 1.2em;
+  }
+  .item-message::before {
+    position: absolute;
+    content: " ";
+    display: block;
+    left: -16px;
+    bottom: 12px;
+    border: 4px solid transparent;
+    border-right: 12px solid #deefe8;
+  }
+  .send-button {
+    height: 4em;
+  }
+  /* トランジション用スタイル */
+  .chat-enter-active {
+    transition: all 1s;
+  }
+  .chat-enter {
+    opacity: 0;
+    transform: translateX(-1em);
+  }
 </style>
+
+<script>
+  // @ is an alias to /src
+  import firebase from "firebase";
+  import Nl2br from "vue-nl2br";
+
+  export default {
+    name: "Home",
+    components: { Nl2br },
+    data() {
+      return {
+        user: {},
+        chat: {},
+        input: ""
+      };
+    },
+    created() {
+      firebase.auth().onAuthStateChanged(user => {
+        this.user = user ? user : {};
+        const ref_message = firebase.database().ref("message");
+        if (user) {
+          this.chat = [];
+
+          ref_message.limitToLast(10).on("child_added", this.childAdded);
+        } else {
+          ref_message.limitToLast(10).off("child_added", this.childAdded);
+        }
+      });
+    },
+    methods: {
+      doLogin() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider);
+      },
+      doLogout() {
+        firebase.auth().signOut();
+      },
+      scrollBottom() {
+        this.$nextTick(() => {
+          window.scrollTo(0, document.body.clientHeight);
+        });
+      },
+
+      childAdded(snap) {
+        const message = snap.val();
+        this.chat.push({
+          key: snap.key,
+          name: message.name,
+          image: message.image,
+          message: message.message
+        });
+        this.scrollBottom();
+      },
+      doSend() {
+        if (this.user.uid && this.input.length) {
+          // firebase にメッセージを追加
+          firebase
+                  .database()
+                  .ref("message")
+                  .push(
+                          {
+                            message: this.input,
+                            name: this.user.displayName,
+                            image: this.user.photoURL
+                          },
+                          () => {
+                            this.input = ""; // フォームを空にする
+                          }
+                  );
+        }
+      }
+    }
+  };
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA83h-9woNOKMLAOQwnrIoqqcAyNJKEVpw",
+  authDomain: "chat-app-89deb.firebaseapp.com",
+  databaseURL: "https://chat-app-89deb-default-rtdb.firebaseio.com",
+  projectId: "chat-app-89deb",
+  storageBucket: "chat-app-89deb.appspot.com",
+  messagingSenderId: "278848793927",
+  appId: "1:278848793927:web:8ebc1cbe3f1c77c5e3b062",
+  measurementId: "G-XQRSK402VG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
